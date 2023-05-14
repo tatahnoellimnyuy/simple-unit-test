dd3a39cc6ccf4abe1c38f135a927be994d5e82e1
tatahnoellimnyuy <tatahnoellimnyuy@gmail.com>
2023-05-14 11:31:03 +0200

    added the unit test

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# FTP server details
+# HOST=""
+# USERNAME=""
+# PASSWORD=""
+echo "deployment passed"
+# # Directory on the FTP server to upload the code to
+# REMOTE_DIR="~/"
+
+# # Local directory containing the PHP code to be deployed
+# LOCAL_DIR="./"
+
+# # FTP deployment using lftp
+# lftp -c "open -u $USERNAME,$PASSWORD $HOST; mirror --reverse --delete --verbose $LOCAL_DIR $REMOTE_DIR"
